bd5b17139b9efc53c366802924c2ef324e552b6e
grahse <gyykim@gmail.com>
2020-10-10 12:36:31 -0700

    Create ProjectOwner component

---
+++ client/src/components/ProjectOwner.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+
+const ProjectOwner = (props) => {
+  return (
+    <div id="project-owner">
+      <h4 className="project-owner-name">{props.owner.name}</h4>
+      <p className="project-owner-backed-created">{props.owner.created} created - {props.owner.backed} backed</p>
+      <p className="project-owner-bio">{props.owner.aboutMe}</p>
+    </div>
+  )
+}
+
+export default ProjectOwner;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,14 +1,46 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+const axios = require('axios');
+import ProjectOwner from './components/ProjectOwner.jsx';
 
 class Pledge extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+      owner: {
+        name: 'Name',
+        created: 0,
+        backed: 0,
+        aboutMe: 'About Me'
+      },
+      options: {}
+    }
+    this.fetchOwner = this.fetchOwner.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchOwner(1);
+  }
+
+  fetchOwner(itemId) {
+    axios.get(`project-owner/${itemId}`)
+      .then((result) => {
+        this.setState({
+          owner: {
+            name: result.data.name,
+            created: result.data.created,
+            backed: result.data.backed,
+            aboutMe: result.data.aboutMe
+          }
+        });
+      });
   }
 
   render() {
     return (
-      <p>pledge</p>
+      <div id="pledge-sidebar">
+        <ProjectOwner owner={this.state.owner}/>
+      </div>
     )
   }
 }
