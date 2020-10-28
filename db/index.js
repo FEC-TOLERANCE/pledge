e0edfe686772fecb0111fea36f6847ab8beba343
grahse <gyykim@gmail.com>
2020-10-27 20:11:20 -0700

    Edit client routes for EC2 deployment

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://mongo:27017/pledge', {useNewUrlParser: true});
+mongoose.connect('mongodb://127.0.0.1:27017/pledge', {useNewUrlParser: true});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
